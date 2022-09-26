1fc3989425382f69f3e08fb5b2b3b406ae1e0843
Slava <vp1891719@gmail.com>
2022-09-26 11:45:21 +0300

    optimization

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,17 @@
-$(function(){
-$('.slider__inner').slick({
-    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
-    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
-    infinite: false,
-    autoplay: true,
-    autoplaySpeed: 5000,
-});
+$(function () {
+    $('.slider__inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
 
-$('select').styler();
+    $('select').styler();
+
+    $('.header__btn-menu').on('click', function () {
+        $('.menu ul').slideToggle();
+    });
+
+    
 });
